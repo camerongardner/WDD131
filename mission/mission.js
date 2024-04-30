562a3790011ac568027f5879a57db91e3652a4ab
camerongardner <gar21019@byui.edu>
2024-04-29 19:49:17 -0600

    Added dark theme

---
+++ mission/mission.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var themeSelector = document.getElementById('theme');
+    var imageSelector = document.querySelector('footer img');
+    themeSelector.addEventListener('change', function() {
+        document.body.classList.toggle('dark', this.value === 'dark');
+        if (this.value === 'dark') {
+            imageSelector.src = './byui-logo_white.png';
+        } else {
+            imageSelector.src = './byui-logo_blue.webp';
+        }
+    });
+});
