b89341b714c50a673a6152d478969a7a5fa46416
camerongardner <gar21019@byui.edu>
2024-05-13 18:57:18 -0600

    Add large viewer functionality.

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -20,6 +20,42 @@ document.getElementById('menu').addEventListener('click', function() {
     }
 });
 
+function viewerTemplate(pic, alt) {
+    return `<div class="viewer" style="display:none;">
+      <span class="close-viewer">X</span>
+      <img src="${pic}" alt="${alt}">
+    </div>`;
+}
+
+function viewHandler(event) {
+    if (event.target.tagName === "IMG") {
+        var clicked = event.target;
+        var imgSrc = clicked.src;
+        var baseName = imgSrc.substring(0, imgSrc.lastIndexOf("-"));
+        var newSrc = `${baseName}-full.jpeg`;
+        var largeImg = viewerTemplate(newSrc, clicked.alt);
+
+        document.body.insertAdjacentHTML("afterbegin", largeImg);
+        document.querySelector(".viewer").style.display = 'flex';
+        document.querySelector(".close-viewer").addEventListener('click', closeViewer);
+    }
+}
+
+document.querySelector('.gallery').addEventListener('click', viewHandler);
+
+function closeViewer() {
+    const viewer = document.querySelector('.viewer');
+    if (viewer) {
+        viewer.remove();
+    }
+}
+
+
+
+
+
+
+  
 
 window.addEventListener('resize', adjustMenuVisibility);
 
